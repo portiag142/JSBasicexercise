78fad358d095291148c8ffeec1b65c04c40079aa
Portia Greene <pd12300@yahoo.com>
2021-09-20 16:04:18 -0400

    finished JSBasicExercise homework

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(13);
+// Numbers, Strings, Booleans, Null, Undefined
+
+// +, -, * , **, %, /
+
+console.log(5%2);
+console.log(1%5);
+
+
+console.log('I\'m Mary Poppins, Y\'all!');
+
+console.log("Hawk" + "Eye");
+
+console.log("Portia".length + "Greene".length);
+
+console.log("Endgame"[0]);
